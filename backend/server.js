43aed6857be1c84b9eab9d56a6b381922e62dfc0
gokulakrishnan <gokulhari182@gmail.com>
2025-06-14 00:37:11 +0530

    Fix incorrect import path for ordersRoutes

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
-const orderRoutes = require('./routes/ordersRoutes'); // ✅ matches file name
+const orderRoutes = require('./routes/Order'); // ✅ matches file name
 
 
 const app = express();
